fe1fd66a0635eaf2885da28c70ebfc67c840fa7a
Felipe Villani <felipevillani.2000@hotmail.com>
2025-08-10 21:01:17 -0300

    feat: nodes and some functions

---
+++ Node.cs
@@ -0,0 +1,62 @@
+﻿namespace binary_tree;
+
+internal class Node(int value)
+{
+    public Node? Left;
+    public Node? Right;
+    public int Value = value;
+
+    public bool IsLeaf() => Left == null && Right == null;
+    public bool IsValid(){
+        
+        if(IsLeaf()) return true;
+
+        if(Left == null || Right == null) return false;
+
+        return Left!.IsValid() && Right!.IsValid();
+    }
+
+    public List<Node> PreOrder()
+    {
+        var preOrder = new List<Node>();
+        preOrder.Add(this);
+        if(Left != null)
+            preOrder.AddRange(Left.PreOrder());
+        if(Right != null) 
+            preOrder.AddRange(Right.PreOrder());
+        return preOrder;
+    }
+    public List<Node> InOrder()
+    {
+        var inOrder = new List<Node>();
+
+        if(Left != null)
+            inOrder.AddRange(Left.InOrder());
+
+        inOrder.Add(this);
+
+        if(Right != null) 
+            inOrder.AddRange(Right.InOrder());
+        return inOrder;
+    }
+    public List<Node> PostOrder()
+    {
+        var postOrder = new List<Node>();
+
+        if(Left != null)
+            postOrder.AddRange(Left.PostOrder());
+
+        postOrder.Add(this);
+
+        if(Right != null) 
+            postOrder.AddRange(Right.PostOrder());
+        return postOrder;
+    }
+
+    public override string ToString()
+    {
+        return this.Value.ToString();
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+using binary_tree;
+using System.ComponentModel.Design;
+
+var root = new Node(10);
+
+root.Left = new Node(5);
+root.Right = new Node(15);
+
+root.Left.Left = new Node(2);
+root.Left.Right = new Node(7);
+
+root.Right.Left = new Node(12);
+root.Right.Right = new Node(17);
+
+root.InOrder().ForEach(x => Console.Write(x + ", "));
+
+
+
+int GetHeight(Node root)
+{
+    if (!root.IsValid())
+        throw new InvalidOperationException("It is not a valid Binary Tree");
+    int height = 0;
+    if (root.IsLeaf())
+        return height;
+    else
+        height++;
+        height = height + GetHeight(root.Left);
+    return height;
+        
+}
